ad30c81374c39cfb10a72bf9df1d4d5ae74c2388
Spencer Lievens <spencer.lievens@sterlingcoin.org.uk>
2017-04-08 20:41:34 +0200

    Increase threads to 4

--- src/hash.h
+++ src/hash.h
@@ -274,7 +274,7 @@ inline int Argon2d_Phase1_Hash(const void *in, void *out) {
     // main configurable Argon2 hash parameters
     context.m_cost = 250; // Memory in KiB (~256KB)
     context.lanes = 4;    // Degree of Parallelism
-    context.threads = 2;  // Threads
+    context.threads = 4;  // Threads
     context.t_cost = 1;   // Iterations
 
     return argon2_core(&context, Argon2_d);
@@ -312,7 +312,7 @@ inline int Argon2d_Phase2_Hash(const void *in, void *out) {
     context.flags = DEFAULT_ARGON2_FLAG; // = ARGON2_DEFAULT_FLAGS
     // main configurable Argon2 hash parameters
     context.m_cost = 250; // Memory in KiB (~250KB)
-    context.lanes = 64;    // Degree of Parallelism
+    context.lanes = 4;    // Degree of Parallelism
     context.threads = 2;  // Threads
     context.t_cost = 1;    // Iterations
     
