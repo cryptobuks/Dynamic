24c267495d31ab71dbb5aff91bbd7de363fd1513
Spencer Lievens <silkcoindeveloper@gmail.com>
2017-04-20 17:29:00 +0200

    Remove "Core" from string

--- src/init.cpp
+++ src/init.cpp
@@ -1679,7 +1679,7 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
                 strErrors << _("Error loading wallet.dat: Wallet requires newer version of Dash Core") << "\n";
             else if (nLoadWalletRet == DB_NEED_REWRITE)
             {
-                strErrors << _("Wallet needed to be rewritten: restart Dynamic Core to complete") << "\n";
+                strErrors << _("Wallet needed to be rewritten: restart Dynamic to complete") << "\n";
                 LogPrintf("%s", strErrors.str());
                 return InitError(strErrors.str());
             }
